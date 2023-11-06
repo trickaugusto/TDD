7f65b953702104b45a1e457e336916651193e00f
trickaugusto <trickaugusto1206@gmail.com>
2023-11-06 17:59:20 -0300

    refactor: static to non static methods

--- src/Model/Service/Avaliador.php
+++ src/Model/Service/Avaliador.php
@@ -13,6 +13,10 @@ class Avaliador
 
     public function avalia(Leilao $leilao): void
     {
+        if (!$leilao->getLances()) {
+            throw new \DomainException('Não é possível avaliar um leilão vazio');
+        }
+
         foreach ($leilao->getLances() as $lance) {
             if ($lance->getValor() > $this->maiorValor) {
                 $this->maiorValor = $lance->getValor();
